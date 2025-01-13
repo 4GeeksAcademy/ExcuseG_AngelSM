4f082dfb482b0b817bc8f26ba27520bf3cff7b2f
Ángel Sánchez Moreno <univ.angel97@gmail.com>
2025-01-13 11:44:38 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.addEventListener("load", myfunction);
+document.getElementById("boton").addEventListener("click", myfunction);
+function myfunction() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"]; //aquí uso 'let'para agregar el sujeto de la excusa
+  let action = ["ate", "peed", "crushed", "broke"]; // aquí esta la acción
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ]; // de aqui para atrás es solo establecer la variable
+
+  //aquí abajo uso las propiedades para que me de un indice aleatorio con las variables anteriormente mencionadas
+  let whoN = Math.floor(Math.random() * who.length);
+  let actionN = Math.floor(Math.random() * action.length);
+  let whatN = Math.floor(Math.random() * what.length);
+  let whenN = Math.floor(Math.random() * when.length);
+
+  document.getElementById("Excuse").innerHTML =
+    who[whoN] + " " + action[actionN] + " " + what[whatN] + " " + when[whenN];
+}
